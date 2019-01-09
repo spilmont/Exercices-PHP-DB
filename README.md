55e8c61f7251d297835c10ad69849c7f8ab4c6ef
spilmont <43751712+spilmont@users.noreply.github.com>
2019-01-09 13:05:13 +0100

    s

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 Consignes :
 
-- Vous allez réaliser votre premier CRUD en PHP : CRUD est l'acronyme de Create Read Update Delete
-- Tout d'abord, vous allez utiliser PHP et l'extension mySQLi pour vous connecter à votre base de donnée sur webhost
-- Vous selectionnerez votre base de donnée
-- Vous allez créer une fonction en PHP qui va ajouter une personne à la table eleves, cette fonction prendra en parametre
++ Vous allez réaliser votre premier CRUD en PHP : CRUD est l'acronyme de Create Read Update Delete
++Tout d'abord, vous allez utiliser PHP et l'extension mySQLi pour vous connecter à votre base de donnée sur webhost
++ Vous selectionnerez votre base de donnée
++ Vous allez créer une fonction en PHP qui va ajouter une personne à la table eleves, cette fonction prendra en parametre
 $nom,$prenom, $age
 
-- A l'intérieur de cette fonction, vous allez executer la requete sql adéquate qui va insérer un nouvel enregistrement
++ A l'intérieur de cette fonction, vous allez executer la requete sql adéquate qui va insérer un nouvel enregistrement
   en utilisant les parametres de votre fonction comme valeurs
 
-- Vous allez créer une fonction qui va selectionner tout les enregistrements de la table eleves et retourner le resultat
-- Vous allez faire un appel à cette fonction pour afficher la liste des éléves sur la page
++Vous allez créer une fonction qui va selectionner tout les enregistrements de la table eleves et retourner le resultat
++Vous allez faire un appel à cette fonction pour afficher la liste des éléves sur la page
 
-- Créer une autre fonction permettant de mettre à jour un éléve, cette fonction prendra en parametre $prenom,$nom,$age
++ Créer une autre fonction permettant de mettre à jour un éléve, cette fonction prendra en parametre $prenom,$nom,$age
    et $idEleve, elle mettre à jour l'éléve ayant pour id $idEleve en utilisant l'instruction SQL adéquate
 
-- Créer une autre fonction permettant de supprimer un éléve, cette fonction prendra en parametre $idEleve et permettra
++ Créer une autre fonction permettant de supprimer un éléve, cette fonction prendra en parametre $idEleve et permettra
    de supprimer l'enregistrement ayant pour id $idEleve en utilisant l'instruction SQL adéquate.
 
 BONUS : Créer toute la partie front qui affichera un formulaire permettant d'ajouter un éléve, un lien permettant d'afficher
--- index.php
+++ index.php
@@ -8,7 +8,242 @@
 
 // Premiere ligne
 
+echo "<h1>Fiche des eleves</h1>";
 
+$servername = "localhost";
+$username = "id7331244_spilmontandre";
+$password = "";
+$dbname = "id7331244_basetest";
+
+$click =0;
+
+$conn = new mysqli($servername,$username,$password);
+
+if($conn ->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}else{
+// Selectionner la base à utiliser
+    $conn->select_db($dbname);
+}
+
+
+
+function eleves($nom, $prenom, $age)
+{
+    global $conn;
+
+    $sql = "INSERT INTO eleves VALUES ('','$prenom','$nom','$age')";
+
+    $conn ->query($sql);
+
+
+    if($conn->query($sql) === TRUE){
+        echo "Les enregistrements ont été ajouté";
+    }else{
+        die("ERREUR: " . $conn->error);
+    }
+}
+
+
+
+
+function afficher(){
+global $conn;
+
+ $sql ="SELECT * FROM eleves";
+
+    $result = $conn->query($sql);
+    while($row = $result->fetch_assoc())
+    {
+        echo $row["id"].".".$row['nom']." ".$row['prenom']." ".$row["age"]."<br>";
+    }
+}
+
+
+
+
+function update($id,$nom,$prenom,$age){
+
+    global $conn;
+
+    $sql = "UPDATE eleves set nom = '$nom', prenom = '$prenom', age = '$age' where id = $id";
+    $conn->query($sql);
+
+
+}
+
+function delete($id){
+    global $conn;
+
+ $sql = "DELETE FROM eleves WHERE id= $id";
+    $conn->query($sql);
+
+
+}
+
+?>
+
+<form action="index.php" method="get">
+    <fieldset>
+        <legend>nouvelle eleves</legend>
+   <input type="text" name="nom" placeholder="entrer un nom">
+   <input type="text" name="prenom" placeholder="entrer un prenom">
+    <input type="text" name="age" placeholder="entrer votre age">
+    <input type="submit" name="envoyer" value="envoyer">
+    </fieldset>
+</form>
+
+
+<?php
+
+
+
+
+
+function formulaireEleve(){
+
+    global $conn;
+    $nom = $_GET['nom'];
+    $prenom = $_GET['prenom'];
+    $age = $_GET['age'];
+
+
+
+
+    if($_GET["nom"] !="" && $_GET['prenom'] != "" && $_GET['age']!=""  ) {
+
+
+        eleves($nom, $prenom, $age);
+
+    }
+else{
+        echo "veuillez entrez des information";
+    }
+}
+formulaireEleve();
+?>
+<hr>
+<form action="index.php" method="get">
+    <input type="submit" name="afficher" value="afficher la liste">
+</form>
+
+<?php
+if(!empty($_GET['afficher'])) {
+    afficher();
+}
+echo "<hr>";
+?>
+
+<form action="index.php" method="get">
+    <fieldset>
+        <legend>suprimer un eleves</legend>
+        <input type="text" name="suprimer" placeholder="entrer ID de l'eleve a supprimer">
+        <input type="submit" name="ids" value="suprimer">
+    </fieldset>
+</form>
+
+<?php
+
+
+
+
+echo "<hr>";
+
+?>
+<form action="index.php" method="get">
+    <fieldset>
+        <legend>modifier les information des eleves</legend>
+        <input type="text" name="id1" placeholder="ID de l'eleve a modifier">
+        <input type="text" name="nom1" placeholder="nom de l'eleve a modifier">
+        <input type="text" name="prenom1" placeholder="prenom de l'eleve a modifier">
+        <input type="text" name="age1" placeholder="ages de leleve a modifier">
+        <input type="submit" name="modifier" value="modifier">
+    </fieldset>
+</form>
+<?php
+
+
+    update($_GET["id1"], $_GET["nom1"], $_GET["prenom1"], $_GET["age1"]);
+
+delete($_GET["suprimer"]);
+
+echo "<hr><hr>"
+?>
+<h1>Fiche Mugs</h1>
+
+<?php
+function mugs($description){
+    global $conn;
+
+    $sql= "INSERT INTO mugs VALUES ('','$description')";
+
+    $conn ->query($sql);
+}
+
+if (!empty($_GET["mugs"])){
+mugs($_GET['mugs']);
+}
+
+?>
+<form action="index.php" method="get">
+    <input type="text" name="mugs">
+    <input type="submit" name="ajouterMugs" value="ajouter un mugs">
+</form>
+
+<?php
+
+function associer($idEleve, $idMugs){
+
+    global $conn;
+
+    $sql="INSERT INTO eleves_mugs value ('',$idEleve,$idMugs)";
+
+    $conn->query($sql);
+
+}
+
+
+function updateMugs($idMugsM,$description){
+
+    global $conn;
+
+    $sql = "UPDATE mugs set description = '$description' where id = $idMugsM";
+
+    $conn->query($sql);
+
+}
+?>
+<form action="index.php" method="get">
+    <input type="text" name="elevesMugs">
+    <input type="submit" name="assossier" value="assossier">
+</form>
+
+<?php
+function afficherassosiation($ideleves){
+    global $conn;
+
+
+    $sql ="SELECT * FROM eleves where id = $ideleves";
+
+    $result = $conn->query($sql);
+    while($row = $result->fetch_assoc())
+    {
+        echo $row["nom"]." ".$row["prenom"]." a ";
+    }
+
+
+    $sql ="SELECT mugs.description FROM eleves_mugs,mugs WHERE eleves_mugs.id_eleves = $ideleves and eleves_mugs.id_mugs = mugs.id";
+    $result = $conn->query($sql);
+    while($row = $result->fetch_assoc())
+    {
+        echo $row["description"].",";
+    }
+
+
+
+}
+
+afficherassosiation($_GET["elevesMugs"]);
 
 
 
